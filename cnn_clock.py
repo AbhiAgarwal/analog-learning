189078fcdce329d4c2622108b02e0dff4098f37e
Max Dumas <max.dumas@bodylabs.com>
2017-08-17 16:32:21 -0400

    Update training

--- cnn_clock.py
+++ cnn_clock.py
@@ -8,43 +8,27 @@ from tflearn.layers.estimator import regression
 
 from tflearn.data_utils import image_preloader
 
+from sklearn.model_selection import train_test_split
+
 import numpy as np
-
-# X, Y = image_preloader('/Users/abhiagarwal/Desktop/cnn/dataset.txt', image_shape=(28, 28), mode='file', categorical_labels=True, normalize=True, grayscale=True)
-
 import imageio
-X = [
-    imageio.imread('clocks/%s.jpg' % (i + 1))[:, :, :3]
-    for i in range(719)
-]
 
-X = np.array(X)
+data = np.array([
+    [
+        np.expand_dims(imageio.imread('clocks/%s/%s.jpg' % (j, i)), axis=-1),
+        np.array([ 1 if i == k else 0 for k in range(720) ])
+    ]
+    for i in range(720)
+    for j in range(2)
+])
 
-Y = [
-    [ 1 if i == j else 0 for i in range(720) ]
-    for j in range(720)
-]
+X = np.stack(data[:, 0])
+y = np.stack(data[:, 1])
 
-Y = np.array(Y)
-
-# testX, testY = image_preloader('/Users/abhiagarwal/Desktop/cnn/dataset_test.txt', image_shape=(28, 28), mode='file', categorical_labels=True, normalize=True, grayscale=True)
-
-testX = [
-    imageio.imread('test/240.jpg')[:, :, :3],
-    imageio.imread('test/600.jpg')[:, :, :3],
-]
-
-testX = np.array(testX)
-
-testY = [
-    [ 1 if i == 240 else 0 for i in range(720) ],
-    [ 1 if i == 600 else 0 for i in range(720) ],
-]
-
-testY = np.array(testY)
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=32)
 
 # Building convolutional network
-network = input_data(shape=[None, 128, 128, 3], name='input')
+network = input_data(shape=[None, 128, 128, 1], name='input')
 network = conv_2d(network, 32, 3, activation='relu', regularizer="L2")
 network = max_pool_2d(network, 2)
 network = local_response_normalization(network)
@@ -56,11 +40,10 @@ network = dropout(network, 0.8)
 network = fully_connected(network, 256, activation='tanh')
 network = dropout(network, 0.8)
 network = fully_connected(network, 720, activation='softmax')
-network = regression(network, optimizer='adam', learning_rate=0.01,
-                     loss='categorical_crossentropy', name='target')
+network = regression(network, optimizer='adam', learning_rate=0.01, loss='categorical_crossentropy', name='target')
 
 # Training
-model = tflearn.DNN(network, tensorboard_verbose=0)
-model.fit({'input': X}, {'target': Y}, n_epoch=20,
-           validation_set=({'input': testX}, {'target': testY}),
+model = tflearn.DNN(network, tensorboard_verbose=1)
+model.fit({'input': X_train}, {'target': y_train}, n_epoch=20,
+           validation_set=({'input': X_test}, {'target': y_test}),
            snapshot_step=100, show_metric=True, run_id='cnn_clocker')
